a8f617f7d02cf081c9b2c701d2bb4d33ceac4d34
jac30b <galeckijakub9@gmail.com>
2021-09-15 22:22:40 +0200

    Move parse

--- scripts/Parser.js
+++ scripts/Parser.js
@@ -3,41 +3,8 @@ const {Request} = require('./Request');
 function Parser(data) {}
 
 Parser.prototype.parse = (data) => {
-    if(data && Parser.isValidData(data)){
-        data = JSON.parse(data);
-        if(Request.isRequest(data)){
-            return new Request(data.message, data.type, data.user, data.room);
-        } 
-    }
-}
-
-Parser.isValidData = (data) => {
-    const getDepth = (tree) => {
-        let depth = 0;
-        if(tree.children) {
-           tree.children.forEach((child) => {
-               let tmp = getDepth(child);
-               if(tmp > depth) {
-                   depth = tmp;
-               }
-           });
-        }
-        return 1 + depth;
-    }
-
-    try {
-        const parsedData = JSON.parse(data);
-        const depth = getDepth(parsedData);
-
-        if(depth != 1) {
-           return false;
-        }
-        
-        return parsedData.hasOwnProperty('type') &&  parsedData.hasOwnProperty('message') 
-                 && parsedData.hasOwnProperty('user') && parsedData.hasOwnProperty('room');
-    
-    } catch (e) {
-        return false;
+    if(Request.isRequest(data)){
+        return Request.parse(data);
     }
 }
 
--- scripts/Request.js
+++ scripts/Request.js
@@ -5,8 +5,43 @@ function Request(message, type, user, room){
     this.room = room;
 }
 
-Request.isRequest = (obj) => {
-    return true;
+Request.isRequest = (data) => {
+    const getDepth = (tree) => {
+        let depth = 0;
+        if(tree.children) {
+            tree.children.forEach((child) => {
+                let tmp = getDepth(child);
+                if(tmp > depth) {
+                    depth = tmp;
+                }
+            });
+        }
+        return 1 + depth;
+    }
+
+    try {
+        const parsedData = JSON.parse(data);
+        const depth = getDepth(parsedData);
+
+        if(depth !== 1) {
+            return false;
+        }
+
+        return parsedData.hasOwnProperty('type') &&  parsedData.hasOwnProperty('message')
+            && parsedData.hasOwnProperty('user') && parsedData.hasOwnProperty('room');
+
+    } catch (e) {
+        return false;
+    }
+}
+
+Request.parse = (data) => {
+    if(data && Request.isRequest(data)){
+        data = JSON.parse(data);
+        if(Request.isRequest(data)){
+            return new Request(data.message, data.type, data.user, data.room);
+        }
+    }
 }
 
 module.exports.Request = Request;
